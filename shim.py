b8a2b6f4c2b7247309ede31237443edbfade0e25
Kabuto-Mainer <snv.bor@gmail.com>
2026-04-09 12:27:29 +0300

    Change shim

--- shim.py
+++ shim.py
@@ -4,19 +4,30 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 led = 26
+GPIO.setup(led, GPIO.OUT)
+
 pwm = GPIO.PWM(led, 200)
 duty = 0.0
 pwm.start(duty)
 
-GPIO.setup(led, GPIO.OUT)
+is_up = True
 
 while True:
     pwm.ChangeDutyCycle(duty)
     time.sleep(0.05)
 
-    duty += 1.0
+    if is_up:
+        duty += 1.0
+    else:
+        duty -= 1.0
+
     if duty > 100.0:
+        duty = 100.0
+        is_up = False
+
+    if duty < 0.0:
         duty = 0.0
+        is_up = True
 
 
 
