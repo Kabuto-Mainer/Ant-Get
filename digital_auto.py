f7426e04150e79a896308ec171899f6cacf76876
Kabuto-Mainer <snv.bor@gmail.com>
2026-04-09 12:18:53 +0300

    delete unused

--- digital_auto.py
+++ digital_auto.py
@@ -13,17 +13,17 @@ while True:
     imp = GPIO.input(foto_check)
     GPIO.output(led, not imp)
 
-button = 13
-state = 0
-sleep_time = 0.2
+# button = 13
+# state = 0
+# sleep_time = 0.2
 
-GPIO.setup(button, GPIO.IN)
-GPIO.setup(led, GPIO.OUT)
+# GPIO.setup(button, GPIO.IN)
+# GPIO.setup(led, GPIO.OUT)
 
-while True:
-    imp = GPIO.input(button)
-    if imp:
-        state = not state
-        GPIO.output(led, state)
+# while True:
+#     imp = GPIO.input(button)
+#     if imp:
+#         state = not state
+#         GPIO.output(led, state)
 
-    time.sleep(sleep_time)
+#     time.sleep(sleep_time)
