5ac50cb57be962a4ae842d4a173127062cbd20ff
Kabuto-Mainer <snv.bor@gmail.com>
2026-04-16 12:58:13 +0300

    6 - taska

---
+++ pwn-sin.py
@@ -0,0 +1,27 @@
+import r2_shim as r2r
+import sin_gen as sg
+import time
+
+
+if __name__ == "__main__":
+    amplitude = 3.2
+    signal_frequency = 10
+    sampling_frequency = 1000
+    t = 0.0
+
+    try:
+        dac = r2r.R2R_Dac(12, 500, 3.2, True)
+
+        while True:
+            # try:
+                voltage = sg.get_sin_wave_ampltd(signal_frequency, t) * amplitude
+                # voltage = float(input("Введите напряжение в Вольтах: "))
+                dac.set_voltage(voltage)
+                sg.wait_for_sampling_period(sampling_frequency)
+                t += 1 / sampling_frequency
+
+            # except ValueError:
+            #     print("Вы ввели не число. Попробуйте ещё раз\n")
+
+    finally:
+        dac.deinit()
