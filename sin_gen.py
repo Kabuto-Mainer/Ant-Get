bc74b68c756975d66adbed1ea7a4ddff0bcda579
Kabuto-Mainer <snv.bor@gmail.com>
2026-04-16 13:20:05 +0300

    8 - task

--- sin_gen.py
+++ sin_gen.py
@@ -8,6 +8,13 @@ def get_sin_wave_ampltd(freq: float, time: float):
     # print(value)
     return value
 
+def get_trig_wave_ampltd(freq: float, time: float):
+    value = time * freq % 1.0
+
+    if value <= 0.5:
+        return value
+    return value - 0.5
+
 def wait_for_sampling_period(freq: float):
     time.sleep(1 / freq)
 
